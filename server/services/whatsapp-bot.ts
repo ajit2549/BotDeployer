6ecad0b017ad823203e11a98f369cfeb0051d0a1
ajit2549 <121962898+ajit2549@users.noreply.github.com>
2025-08-23 17:08:13 +0000

    Revert uptime

--- server/services/whatsapp-bot.ts
+++ server/services/whatsapp-bot.ts
@@ -4,18 +4,8 @@ import qrcode from "qrcode";
 import fetch from "node-fetch";
 import Tesseract from "tesseract.js";
 import { storage } from "../storage";
-import express from "express"; 
 
 // ------------------- Setup Express -------------------
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (_req, res) => {
-  res.send("🌍 WhatsApp Bot is running!");
-});
-app.listen(PORT, () => {
-  console.log(`🌍 Keep-alive server running at http://localhost:${PORT}`);
-});
 
 interface BotStatus {
   isRunning: boolean;
